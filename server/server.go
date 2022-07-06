6d3d4bc1e2b77bf9a96bae8ad82cdc922fac2111
Michael <michaelgbenle@gmail.com>
2022-07-06 20:05:18 +0100

    Boiler plate code

--- server/server.go
+++ server/server.go
@@ -1,1 +1,20 @@
 package server
+
+import "log"
+
+func Start() error {
+	values := database.InitializeDbParameters()
+	var PDB = new(database.PostgresDb)
+	h := &handler.Handler{DB: PDB}
+
+	err := PDB.SetupDb(values.Host, values.User, values.Password, values.DbName, values.Port)
+	if err != nil {
+		log.Fatal(err)
+	}
+	routes, port := router.SetupRouter(h)
+	err = routes.Run(":" + port)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return nil
+}
