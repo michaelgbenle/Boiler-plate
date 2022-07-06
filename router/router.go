2dd52c9a1d22b95d441a2f8be13b5fed29a55c80
Michael <michaelgbenle@gmail.com>
2022-07-06 20:36:38 +0100

    Boiler plate code

--- router/router.go
+++ router/router.go
@@ -1,1 +1,19 @@
 package router
+
+import (
+	"github.com/gin-gonic/gin"
+	"os"
+)
+
+func SetupRouter(h *handler.Handler) (*gin.Engine, string) {
+	router := gin.Default()
+	router.GET("/customer", h.GetCustomer)
+	router.GET("/transactions", h.GetTransaction)
+	router.PATCH("/credit", h.CreditWallet)
+	router.PATCH("/debit", h.DebitWallet)
+	router.POST("/addcustomer", h.AddCustomer)
+
+	port := os.Getenv("PORT")
+
+	return router, port
+}
