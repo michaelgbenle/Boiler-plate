7316a3814b2bca6d73de37c40f33584f7a670dc0
Michael <michaelgbenle@gmail.com>
2022-07-06 20:07:02 +0100

    Boiler plate code

--- database/db_interface.go
+++ database/db_interface.go
@@ -1,1 +1,43 @@
 package database
+
+import (
+	"log"
+	"os"
+)
+
+type DB interface {
+	Getcustomer(accountNos string) (*models.Customer, error)
+	Addcustomer(customer models.Customer) error
+	Creditwallet(money *models.Money) (*models.Transaction, error)
+	Debitwallet(money *models.Money) (*models.Transaction, error)
+	Gettransaction(id string) (*[]models.Transaction, error)
+	CreateTransaction(transaction *models.Transaction)
+}
+
+type DbParameters struct {
+	Host     string
+	User     string
+	Password string
+	DbName   string
+	Port     string
+}
+
+func InitializeDbParameters() DbParameters {
+	errEnv := godotenv.Load()
+	if errEnv != nil {
+		log.Fatal("Error loading .env file")
+	}
+	host := os.Getenv("DB_HOST")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+	port := os.Getenv("DB_PORT")
+
+	return DbParameters{
+		Host:     host,
+		User:     user,
+		Password: password,
+		DbName:   dbName,
+		Port:     port,
+	}
+}
