18d08d3765be44c3948f4032c112b3f4be6fcbb1
Michael <michaelgbenle@gmail.com>
2022-07-06 20:25:53 +0100

    Boiler plate code

--- database/database.go
+++ database/database.go
@@ -1,1 +1,29 @@
 package database
+
+import (
+	"fmt"
+	"log"
+	"sync"
+)
+
+type PostgresDb struct {
+	DB *gorm.DB
+}
+
+var mu sync.Mutex
+
+//SetupDb sets up database and auto migrate schema
+func (pdb *PostgresDb) SetupDb(host, user, password, dbName, port string) error {
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Africa/Lagos", host, user, password, dbName, port)
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	pdb.DB = db
+
+	dberr := pdb.DB.AutoMigrate(&models.Customer{}, models.Transaction{})
+	if dberr != nil {
+		log.Fatal(dberr)
+	}
+	return nil
+}
